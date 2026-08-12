c604987fc92bc5415ab82d30352a0855b3514195
ZapadiyaMilan <zapadiyamilan003@gmail.com>
2026-08-12 20:12:59 +0530

    Update banned user contact and progress bar footer

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -39,7 +39,7 @@ async def subscribe(app, message):
       try:
          user = await app.get_chat_member(update_channel, message.from_user.id)
          if user.status == "kicked":
-            await message.reply_text("You are Banned. Contact -- @devgaganin")
+            await message.reply_text("You are Banned. Contact -- @Zapadiya_milan_003")
             return 1
       except UserNotParticipant:
         caption = f"Join our channel to use the bot"
@@ -250,7 +250,7 @@ async def progress_callback(current, total, progress_message):
     f"│ **__Progress:__** {percent:.2f}%\n"
     f"│ **__Uploaded:__** {current_mb:.2f} MB / {total_mb:.2f} MB\n"
     f"╰──────────────────╯\n\n"
-    f"**__Powered by Team SPY__**"
+    f"**__Powered by Team SHELLBY CHENNAL__**"
         )
 
         last_update_time = current_time
