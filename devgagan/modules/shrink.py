1bab2ba7f950f7ddf69f8debae30aa4a1f1f748c
ZapadiyaMilan <zapadiyamilan003@gmail.com>
2026-08-13 14:49:53 +0530

    Change channel and premium button URLs in shrink.py
    Updated channel and premium button URLs in the message reply.

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -74,8 +74,8 @@ async def token_handler(client, message):
     user_id = message.chat.id
     if len(message.command) <= 1:
         image_url = "https://i.postimg.cc/v8q8kGyz/startimg-1.jpg"
-        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/team_spy_pro")
-        premium = InlineKeyboardButton("Get Premium", url="https://t.me/kingofpatal")   
+        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/team_shellby_limited")
+        premium = InlineKeyboardButton("Get Premium", url="https://t.me/Zapadiya_milan_003")   
         keyboard = InlineKeyboardMarkup([
             [join_button],   
             [premium]    
@@ -84,7 +84,7 @@ async def token_handler(client, message):
         await message.reply_photo(
             msg.photo.file_id,
             caption=(
-                "Hi 👋 Welcome, Wanna intro...?\n\n"
+                "Hi 👋 Welcome, SHELLBY ...?\n\n"
                 "✳️ I can save posts from channels or groups where forwarding is off. I can download videos/audio from YT, INSTA, ... social platforms\n"
                 "✳️ Simply send the post link of a public channel. For private channels, do /login. Send /help to know more."
             ),
