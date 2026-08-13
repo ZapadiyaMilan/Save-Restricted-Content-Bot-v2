c78afb4756e9079fa14f9be72ee3a40ee1f5eed4
ZapadiyaMilan <zapadiyamilan003@gmail.com>
2026-08-13 15:08:54 +0530

    Uncomment welcome message in reply_photo

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -84,7 +84,7 @@ async def token_handler(client, message):
         await message.reply_photo(
             msg.photo.file_id,
             caption=(
-             #   "Hi 👋 Welcome, SHELLBY ...?\n\n"
+                "Hi 👋 Welcome, SHELLBY ...?\n\n"
               #  "✳️ I can save posts from channels or groups where forwarding is off. I can download videos/audio from YT, INSTA, ... social platforms\n"
                # "✳️ Simply send the post link of a public channel. For private channels, do /login. Send /help to know more."
             ),
