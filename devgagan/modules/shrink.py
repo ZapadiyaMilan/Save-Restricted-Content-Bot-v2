8ffe9d650c8897ce9e1eecdcb2a8632e8b1d581f
ZapadiyaMilan <zapadiyamilan003@gmail.com>
2026-08-16 13:56:03 +0530

    Update image URL in shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -73,7 +73,7 @@ async def token_handler(client, message):
     msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "Https://i.postimg.cc/bYn891kS/Shelby-on-top.jpg"
+        image_url = "https://i.postimg.cc/FHXKwcht/Shelby-on-top.jpg"
         join_button = InlineKeyboardButton("Join Channel", url="https://t.me/team_shellby_limited")
         premium = InlineKeyboardButton("💬 OWNER CONTACT", url="https://t.me/Zapadiya_milan_003")   
         keyboard = InlineKeyboardMarkup([
